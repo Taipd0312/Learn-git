5a9cd8fd9ff6e9fcb02d43f575f379d710930b66
tai.phan <phanductai312@gmail.com>
2022-10-14 20:11:27 +0700

    [core]: setup repo

---
+++ src/Api/Api.cs
@@ -0,0 +1,15 @@
+using (resource)
+{
+    /// <summary>
+    /// Gets or sets the name of the resource
+    /// <summary>
+    resource.Release;
+    #region namespace
+        hello = resource.GetType().GetGenericArguments().FirstOrDefault();
+    #endregion
+
+    public static void main(string[] args)
+    {
+        
+    }
+}
